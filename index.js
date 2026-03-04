5db5399d58a6b356944e4652394013ef0a90f017
yeisonpopo369 <yeisonpopo369@gmail.com>
2026-03-04 14:31:02 -0400

    agregado express

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot activo 24/7");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor web activo");
+});
 const {
   Client,
   GatewayIntentBits,
